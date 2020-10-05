707bdef29ae398c5021a1358d17158b2a22a29d3
mastachi-cin <cintia_mmm@icloud.com>
2020-10-04 22:32:41 -0500

    Final version

--- Plotly/static/js/app.js
+++ Plotly/static/js/app.js
@@ -1,17 +1,32 @@
 
+// Global variable for sample data
 var main_data = [];
 
+// Global variable for metadata
+var main_metadata = [];
+
+// Importa data from json file
 d3.json("data/samples.json").then((importedData) => {
 
-  // Only samples required
+  // Samples required
   var samples = importedData.samples;
 
+  // Metadata required
+  var metadata = importedData.metadata;
+
+  // Global data for samples
   main_data = samples;
+
+  // Global data for metadata
+  main_metadata = metadata;
+
+  // Slice the first object for init Demographic info
+  buildDemoInfo(metadata.slice(0, 1));
 
   // Slice the first object for init bar chart plotting
   buildBarChart(samples.slice(0, 1));
   
-  // Get all ids from samples
+  // Get all ids from samples for bulding dropdown menu
   builDropDownMenu(samples.map(obj => obj.id));
   
   // Slice the first object for init bubble chart
@@ -21,18 +36,19 @@ d3.json("data/samples.json").then((importedData) => {
 
 function buildBarChart(sample) {
 
+  // Get the top 10 OTUs IDs and reverse them from higher to lower
   var otu_ids = sample[0].otu_ids.slice(0,10).reverse();
-  //console.log(otu_ids);
 
+  // Convert the top 10 OTUs IDs into a string 
   var otu_ids_str = otu_ids.map(function(e){return "OTU " + e.toString()});
-  //console.log(otu_ids_str)
 
+  // Get the top 10 OTUs sample values and reverse them from higher to lower
   var values = sample[0].sample_values.slice(0,10).reverse();
-  //console.log(values);
 
+  // Get the top 10 OTUs labels and reverse them from higher to lower
   var labels = sample[0].otu_labels.slice(0,10).reverse();
 
-    // Create the Trace, only the top 10 OTUs
+    // Create the Trace
   var trace1 = {
     x: values,
     y: otu_ids_str,
@@ -57,7 +73,7 @@ function buildBarChart(sample) {
 
 function builDropDownMenu(ids) {
 
-  // Get reference to the Country menu
+  // Get reference to the dropdown menu
   var menuID = d3.select("#selDataset");
 
   // Remove any previous options from the dropdown menu
@@ -76,15 +92,21 @@ d3.selectAll("#selDataset").on("change", optionChanged);
 
 // Function called by DOM changes
 function optionChanged() {
+
+   // Get reference to the dropdown menu
   var dropdownMenu = d3.select("#selDataset");
   
   // Assign the value of the dropdown menu option to a variable
   var selId = dropdownMenu.property("value");
 
-  // Initialize an empty array for the country's data
+  // Filter samples data for selected ID
   var selectedid = main_data.filter(row => row.id === selId);
 
-  console.log(selectedid);
+  // Filter metadata data for selected ID
+  var selectedidMD = main_metadata.filter(row => row.id === parseInt(selId));
+
+  // Update panel with new selected data
+  buildDemoInfo(selectedidMD);
 
   // Update bar char with new selected data
   updateBarChart(selectedid);
@@ -96,14 +118,19 @@ function optionChanged() {
 
 function updateBarChart(selectedid) {
 
+  // Get the top 10 OTUs IDs and reverse them from higher to lower
   var otu_ids = selectedid[0].otu_ids.slice(0,10).reverse();
   
+  // Convert the top 10 OTUs IDs into a string 
   var otu_ids_str = otu_ids.map(function(e){return "OTU " + e.toString()});
 
+  // Get the top 10 OTUs sample values and reverse them from higher to lower
   var values = selectedid[0].sample_values.slice(0,10).reverse();
 
+  // Get the top 10 OTUs labels and reverse them from higher to lower
   var labels = selectedid[0].otu_labels.slice(0,10).reverse();
 
+  // Create the object with the data to be updated
   var newdata = {
     x: [values],
     y: [otu_ids_str],
@@ -116,14 +143,16 @@ function updateBarChart(selectedid) {
 
 function buildBubbleChart(sample) {
 
+  // Get OTUs IDs
   var otu_ids = sample[0].otu_ids;
-  //console.log(otu_ids);
 
+  // Get OTUs sample values
   var values = sample[0].sample_values;
-  //console.log(values);
 
+  // Get OTUs labels
   var labels = sample[0].otu_labels;
 
+  // Create trace
   var trace1 = {
     x: otu_ids,
     y: values,
@@ -135,26 +164,31 @@ function buildBubbleChart(sample) {
     }
   };
   
+  // Create the data array for the plot
   var data = [trace1];
   
+  // Define the plot layout
   var layout = {
     xaxis: { title: "OTU ID"},
     showlegend: false
   };
   
+  // Plot the chart to a div tag with id "bubble"
   Plotly.newPlot('bubble', data, layout);
 }
 
 function updateBubbleChart(selectedid) {
 
+  // Get OTUs IDs
   var otu_ids = selectedid[0].otu_ids;
-  //console.log(otu_ids);
 
+  // Get OTUs sample values
   var values = selectedid[0].sample_values;
-  //console.log(values);
 
+  // Get OTUs labels
   var labels = selectedid[0].otu_labels;
 
+  // Create the object with the data to be updated
   var newdata = {
     x: [otu_ids],
     y: [values],
@@ -167,5 +201,18 @@ function updateBubbleChart(selectedid) {
 
   // Call function to update the chart
   Plotly.restyle("bubble", newdata);
+
+}
+
+function buildDemoInfo(metadata) {
+
+    // Get reference to panel-body
+    var panelData = d3.select("#sample-metadata");
+
+    // remove any previous info
+    panelData.html("");
+
+    // Add key-value pair for metadata to panel
+    Object.entries(metadata[0]).forEach(([key, value]) => panelData.append("div").text(`${key}: ${value}`))
 
 }
